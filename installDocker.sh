33bdd92658fa66f5422c34528d1e71bd9d6a129e
yavor-princeberk <yavor@princeberk.com>
2018-10-30 12:28:12 +0200

    new changes

--- installDocker.sh
+++ installDocker.sh
@@ -1,7 +1,15 @@
 #!/bin/bash 
+CURUSER=$(whoami)
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get install -y software-properties-common python-software-properties
 sudo apt-get update
 sudo  apt-get install apt-transport-https
 sudo apt-get install -y docker-ce
+sudo usermod -aG docker $CURUSER
+read -p "Press Y to reboot the PC " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    sudo reboot
+fi 
